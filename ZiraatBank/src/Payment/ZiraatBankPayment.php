cb2966d8161e6b0dcb02eb4bed26972b6d00f259
Hamed <115875025+aghaeian@users.noreply.github.com>
2024-11-04 20:21:20 +0300

    Update ZiraatBankPayment.php

--- ZiraatBank/src/Payment/ZiraatBankPayment.php
+++ ZiraatBank/src/Payment/ZiraatBankPayment.php
@@ -1,1 +1,39 @@
+<?php
+
+namespace Webkul\ZiraatBank\Payment;
+
+use Illuminate\Support\Facades\Storage;
+use Webkul\Payment\Payment\Payment;
+
+class ZiraatBankPayment extends Payment
+{
+    /**
+     * Payment method code
+     *
+     * @var string
+     */
+    protected $code = 'ziraat_bank';
+
+    /**
+     * Return ZiraatBank redirect url
+     *
+     * @var string
+     */
+    public function getRedirectUrl()
+    {
+        return route('ziraat_bank.payment.redirect');
+    }
+
+    /**
+     * Returns payment method image
+     *
+     * @return array
+     */
+    public function getImage()
+    {
+        $url = $this->getConfigData('image');
+
+        return $url ? Storage::url($url) : bagisto_asset('images/review.png', 'shop');
+    }
+}
 
