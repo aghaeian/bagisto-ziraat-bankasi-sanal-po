0da5a97ff5c3ba44bb0720d2ffc7cb3dd448d924
Hamed <115875025+aghaeian@users.noreply.github.com>
2024-11-05 17:45:37 +0300

    Update paymentmethods.php

--- src/Config/paymentmethods.php
+++ src/Config/paymentmethods.php
@@ -4,8 +4,8 @@ return [
     'ziraat_bank' => [
         'code'        => 'ziraat_bank',
         'title'       => 'Ziraat Bankası',
-        'description' => 'Ziraat Bankası Payment',
-        'class'       => 'Webkul\Ziraat Bankası\Payment\Ziraat BankasıPayment',
+        'description' => 'Ziraat Bankası Ödeme Yöntemi',
+        'class'       => 'Aghaeian\ZiraatBank\Payment\ZiraatBankPayment',
         'active'      => false,
         'sort'        => 1,
         'sandbox'     => false,
